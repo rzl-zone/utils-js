633b117b5d66c95c8b7e7b2e7102e59497f9c458
dwikyputra98 <133492532+dwikyputra98@users.noreply.github.com>
2025-08-06 12:24:23 +0700

    fix: add overloads and ensure correct type guard for `isSet`

--- src/predicates/new/extends-lodash/isSet.ts
+++ src/predicates/new/extends-lodash/isSet.ts
@@ -14,7 +14,9 @@
  * isSet(new WeakSet);
  * // => false
  */
-export function isSet(value: unknown): value is Set<any> {
+export function isSet<T = any>(value: Set<T>): value is Set<T>;
+export function isSet(value: unknown): value is Set<any>;
+export function isSet(value: unknown): value is Set<unknown> {
   return (
     Object.prototype.toString.call(value) === "[object Set]" ||
     value instanceof Set
