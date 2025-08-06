2a8000c17fcb9b4619377bf9bf0bedb7021bb003
dwikyputra98 <133492532+dwikyputra98@users.noreply.github.com>
2025-08-06 12:04:26 +0700

    fix: improve `isObjectOrArray` generic narrowing types and support overload to handle unknown with generic class

--- src/predicates/new/isObjectOrArray.ts
+++ src/predicates/new/isObjectOrArray.ts
@@ -6,15 +6,26 @@ import { isNil } from "@/index";
  *
  * Will return `false` for `null`, `undefined`, and primitives.
  *
- * @param {unknown} val - The value to check.
- * @returns {val is object} Returns `true` if the value is an object or array.
+ * @param {unknown} value - The value to check.
+ * @returns {value is object} Returns `true` if the value is an object or array.
  *
  * @example
  * isObjectOrArray({ name: "Alice" }); // true
  * isObjectOrArray([1,2,3]);           // true
  * isObjectOrArray(null);              // false
+ * isObjectOrArray(undefined);         // false
  * isObjectOrArray("hello");           // false
  */
-export const isObjectOrArray = <T>(val: T): val is NonNullable<T> => {
-  return typeof val === "object" && !isNil(val);
-};
+export function isObjectOrArray(value: null | undefined): false;
+export function isObjectOrArray(
+  value: unknown
+): value is unknown[] | Record<string, unknown>;
+export function isObjectOrArray<T>(
+  value: T
+): value is Extract<
+  Exclude<T, null | undefined>,
+  Record<string, unknown> | unknown[]
+>;
+export function isObjectOrArray(value: unknown): boolean {
+  return typeof value === "object" && !isNil(value);
+}
